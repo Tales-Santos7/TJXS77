ea75f93c8d32e94758d7f9e22a1117a4da1ec98b
Tales Santos <detysantosos61@gmail.com>
2025-08-09 13:31:06 -0300

    Upgrade files

--- www/js/VoltarPopup.js
+++ www/js/VoltarPopup.js
@@ -40,7 +40,7 @@ window.addEventListener("DOMContentLoaded", () => {
         painel: "https://demo-painel-portfolio-digital.vercel.app/",
         detalhe: `detalhes.html?id=portfolio-digital&from=popup`,
         arquivo: "/downloads/portfolio-digital.zip",
-        preco: 40000,
+        preco: 400,
       },
       
       // outros produtos aqui...
